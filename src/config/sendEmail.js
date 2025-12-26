ef57b0d0d57a0403bb47390d9ad731d80091373c
Jatin Chaudhary <09jatin2005@gmail.com>
2025-12-26 21:53:30 +0530

    testing for email sending in prod

--- src/config/sendEmail.js
+++ src/config/sendEmail.js
@@ -1,21 +1,26 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
+    service: "gmail",
     host: "smtp.gmail.com",
-    port: 465,            
-    secure: true,         
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-
+    tls: {
+      ciphers: "SSLv3",
+    },
     pool: true,
     maxConnections: 3,
     maxMessages: 100,
     rateLimit: 5,
-    connectionTimeout: 20000, 
-    socketTimeout: 20000,
+    connectionTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   return transporter.sendMail({
